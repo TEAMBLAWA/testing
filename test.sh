2752e4ef42e38e6a6d442d96a7a8c7c25169bf3d
Anthony Kawa <anthonyk2020@gmail.com>
2021-03-04 23:54:31 -0800

    removed test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-if [[ $GITHUB_EVENT_NAME != 'pull_request' &&
-($GITHUB_REF == 'refs/heads/release'||
-$GITHUB_REF == '\\refs\\heads\\release') ]]; then
-  SHOULD_RELEASE=true;
-else
-  SHOULD_RELEASE=false;
-fi
-echo "SHOULD_RELEASE: $SHOULD_RELEASE"
